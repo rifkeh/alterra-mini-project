36dd8f4d94a98fb073a33837fd321e20a745c16d
Rifkhi Akbar <rifkhihebat@upi.edu>
2023-05-01 16:39:13 +0700

    finish teacher_controller

--- route/route.go
+++ route/route.go
@@ -1,10 +1,16 @@
 package route
 
 import (
+	"miniproject/controller"
+
 	"github.com/labstack/echo/v4"
 )
 
 func New() *echo.Echo{
 	e := echo.New()
+	e.GET("/teachers", controller.GetTeachersController)
+	e.POST("/teacher", controller.CreateTeacherController)
+	e.PUT("/teacher/:id", controller.UpdateTeacherController)
+	e.DELETE("/teacher/:id", controller.DeleteTeacherController)
 	return e
 }
